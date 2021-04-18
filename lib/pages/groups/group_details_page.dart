09a45029d173f8eac09715aec7435748a17252df
Bharat Kathi <bharat1031@gmail.com>
2021-04-18 06:01:40 -0700

    group page event list

--- lib/pages/groups/group_details_page.dart
+++ lib/pages/groups/group_details_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:table/models/group.dart';
 import 'package:table/models/user.dart';
+import 'package:intl/intl.dart';
 import 'package:table/pages/groups/chat_page.dart';
 import 'package:table/pages/groups/new_group_dialog.dart';
 import 'package:table/utils/config.dart';
@@ -21,12 +22,15 @@ class _GroupDetailsPageState extends State<GroupDetailsPage> {
 
   Group group = new Group();
 
+  List<String> events = [];
+
   int page = 0;
   PageController pageController = new PageController();
 
   _GroupDetailsPageState(String id) {
     group.id = id;
     getGroupDetails();
+    getEvents();
   }
 
   void getGroupDetails() {
@@ -43,6 +47,14 @@ class _GroupDetailsPageState extends State<GroupDetailsPage> {
           });
         });
       }
+    });
+  }
+
+  void getEvents() {
+    FirebaseDatabase.instance.reference().child("groups").child(group.id).child("events").onChildAdded.listen((event) {
+      setState(() {
+        events.add("${event.snapshot.value["time"]}–${event.snapshot.value["status"]}");
+      });
     });
   }
 
@@ -121,7 +133,69 @@ class _GroupDetailsPageState extends State<GroupDetailsPage> {
                 },
                 children: [
                   Container(
-
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Events",
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: currTextColor
+                          ),
+                        ),
+                        Padding(padding: EdgeInsets.all(4),),
+                        events.isEmpty ? Center(
+                          child: Container(
+                            padding: EdgeInsets.all(16),
+                            child: Text("Nothing new to see here!", style: TextStyle(color: currDividerColor),),
+                          ),
+                        ) : Expanded(
+                          child: ListView.builder(
+                            itemCount: events.length,
+                            itemBuilder: (context, index) => Container(
+                              padding: EdgeInsets.only(bottom: 8),
+                              child: Card(
+                                color: currCardColor,
+                                child: InkWell(
+                                    borderRadius: BorderRadius.circular(8),
+                                    onTap: () {
+                                      router.navigateTo(context, "/groups/${currUser.id}/group1/events/event1", transition: TransitionType.native);
+                                    },
+                                    child: Container(
+                                      padding: EdgeInsets.all(8),
+                                      child: Row(
+                                        children: [
+                                          Container(padding: EdgeInsets.all(16), child: Icon(Icons.event, color: currDividerColor)),
+                                          Expanded(
+                                            child: Column(
+                                              crossAxisAlignment: CrossAxisAlignment.start,
+                                              mainAxisAlignment: MainAxisAlignment.center,
+                                              children: [
+                                                Text(DateFormat.MEd().format(DateTime.parse(events[index].split("–")[0])), style: TextStyle(color: currTextColor, fontSize: 20)),
+                                                Padding(padding: EdgeInsets.all(2),),
+                                                Text(events[index].split("–")[1], style: TextStyle(color: currDividerColor, fontSize: 16),),
+                                              ],
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    )
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: double.infinity,
+                          child: CupertinoButton(
+                            child: Text("Add Event"),
+                            color: mainColor,
+                            onPressed: () {},
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                   ChatPage(group.id),
                   Container(
